cb8d053aabaf3b07e1a148c5f77e6b021335207f
sofani <sofanib@gmail.com>
2017-01-19 13:20:58 -0800

    updated for second time

--- js/script.js
+++ js/script.js
@@ -28,8 +28,8 @@ var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+//var menuItemsUrl =
+  //"https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
